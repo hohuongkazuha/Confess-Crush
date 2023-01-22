165b969dba3373c4ec8fa46fd794c6ff6d7c0728
hohuongkazuha <118617592+hohuongkazuha@users.noreply.github.com>
2023-01-23 02:51:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Gửi tới người eo tương lai<333",
-    introTitle: 'Xin Chào Cậu NGUYỄN THỊ VÂN',
-    introDesc: `Bánh chuối cần nướng
-    Đậu phụ cần tương
-    Dù em phi thường
-    Vẫn cần chị thương`,
-    btnIntro: 'nhấn vô đây đi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Chị đồng ý làm người iu cụa em được hông?? ><',
-    btnYes: 'YES<33',
-    btnNo: 'NO NEVER',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho Hưn',
-    reply: 'chị đồng ý',
-    mess: 'em biết mà 🥰.em iu chị gất nhìu 😘😘',
-    messDesc: 'vậy là từ nay chị là người yêu của em rồi đó nhé!!!',
-    btnAccept: 'um oke!!',
-    messLink: 'https://www.facebook.com/luu.nguyethuong.7?mibextid=ZbWKwL' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "HAPPY NEW YEAR",
+    introTitle: 'Xin chào TRẦN KHÁNH DUY IT-VJ HUSTER',
+    introDesc: `Với trái tim ấm áp,
+    tôi chúc cậu năm mới vui vẻ, thành công. Happy New Year.`,
+    btnIntro: '^^HiHi^^',
+    title: 'Chúc cậu năm mới thành công, đạt được những dự định của bản thân.🥰',
+    desc: 'Cậu có vui khi biết tôi không dọ hmm? ',
+    btnYes: 'Có chứ <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tôi hy vọng cậu sẽ trải qua một năm mới khỏe mạnh, vui tươi và vững vàng với mọi dự định của bản thân. <3',
+    btnReply: 'Gửi cho tôi',
+    reply: 'rốt cuộc bạn muốn cái gì ?',
+    mess: 'K mún j cả 🥰. Hy vọng có thể cùng tranh biện với cậu khi tôi còn có thể 😘😘',
+    messDesc: '',
+    btnAccept: 'Mong mọi điều thuận lợi sẽ đến với c trong năm mới. Nếu c muốn tìm một người để chia sẻ mọi chuyện trong năm tới, t vẫn luôn ở đây.<3',
+    messLink: 'https://www.facebook.com/luu.nguyethuong.7' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
+
