df73ce5da5e0fc9d40597be77eb47f73b3966127
hohuongkazuha <118617592+hohuongkazuha@users.noreply.github.com>
2023-02-01 07:23:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,2 @@
-const CONFIG = {
-    titleWeb: "HAPPY NEW YEAR",
-    introTitle: 'Xin chào TRẦN KHÁNH DUY IT-VJ HUSTER',
-    introDesc: `Với trái tim ấm áp,
-    tôi chúc cậu năm mới vui vẻ, thành công. Happy New Year.`,
-    btnIntro: '^^HiHi^^',
-    title: 'Chúc cậu năm mới thành công, đạt được những dự định của bản thân.🥰',
-    desc: 'Cậu có vui khi biết tôi không dọ hmm? ',
-    btnYes: 'Có chứ <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tôi hy vọng cậu sẽ trải qua một năm mới khỏe mạnh, vui tươi và vững vàng với mọi dự định của bản thân. <3',
-    btnReply: 'Gửi cho tôi',
-    reply: 'rốt cuộc bạn muốn cái gì ?',
-    mess: 'K mún j cả 🥰. xin chào cậu thoy 😘😘',
-    messDesc: 'ò!!!',
-    btnAccept: 'Mong mọi điều thuận lợi sẽ đến với c trong năm mới. Nếu c muốn tìm một người để chia sẻ mọi chuyện trong năm tới, t vẫn luôn ở đây.<3',
-    messLink: 'https://www.facebook.com/luu.nguyethuong.7' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+
 
